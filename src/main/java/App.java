6d930718eaa04fd85845300e1eb1c6dba26f8189
ecko <eckowen@gmail.com>
2020-11-29 20:08:13 +0500

    done

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,13 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld beanWorldOne = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld beanWorldTwo = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat beanCatOne = (Cat) applicationContext.getBean("cat");
+        Cat beanCatTwo = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanWorldOne.equals(beanWorldTwo));
+        System.out.println(beanCatOne.equals(beanCatTwo));
+
+
     }
 }
